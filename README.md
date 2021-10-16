74c274917e4eb885d5a16be1abd8ba917ca66100
Mário Monteiro <marioammonteiro@gmail.com>
2021-10-16 22:22:10 +0100

    project working after library stop working, and now with a raw HttpURLConnection post request

--- README.md
+++ README.md
@@ -18,3 +18,11 @@ The workspace contains two folders by default, where:
 ## Dependency Management
 
 The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
+
+## Launch project
+
+open vscode and launch debugger or just press F5
+
+get the command from terminal, this can be launched
+
+/usr/bin/env /usr/lib/jvm/java-11-openjdk-arm64/bin/java -agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=localhost:33405 -Djava.library.path=/usr/local/lib -Dfile.encoding=UTF-8 App 
--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+
 import pt.gov.cartaodecidadao.*;
 
 public class App {
+
+  // private String serverUri = "https://192.168.31.206/graphql";
 
   static {
     try {
@@ -34,6 +49,31 @@ public class App {
             PTEID_EId eid = card.getID();
             Citizen citizen = new Citizen(eid);
             System.out.println(citizen);
+// send request
+// URL url = new URL("https://reqres.in/api/users");
+// HttpURLConnection con = (HttpURLConnection) url.openConnection();
+// con.setRequestProperty("Content-Type", "application/json; utf-8");
+// con.setRequestMethod("POST");
+// con.setRequestProperty("Accept", "application/json");
+// con.setDoOutput(true);
+// String jsonInputString = "{\"name\": \"Upendra\", \"job\": \"Programmer\"}";
+// try (OutputStream os = con.getOutputStream()) {
+//   byte[] input = jsonInputString.getBytes("utf-8");
+//   os.write(input, 0, input.length);
+// };
+URL url = new URL("https://reqbin.com/echo/post/json");
+HttpURLConnection http = (HttpURLConnection)url.openConnection();
+http.setRequestMethod("POST");
+http.setDoOutput(true);
+http.setRequestProperty("Accept", "application/json");
+http.setRequestProperty("Authorization", "Bearer {token}");
+http.setRequestProperty("Content-Type", "application/json");
+String data = "{\n	\"employee\":{ \"name\":\"Emma\", \"age\":28, \"city\":\"Boston\" }\n} ";
+byte[] out = data.getBytes(StandardCharsets.UTF_8);
+OutputStream stream = http.getOutputStream();
+stream.write(out);
+System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+http.disconnect();
           } else {
             System.err.println("no card found");
           }
@@ -49,4 +89,29 @@ public class App {
       System.out.println(e);
     }
   }
+
+  // /**
+  // * Examples and Recipes
+  // * https://openjdk.java.net/groups/net/httpclient/recipes.html
+  // */
+  // public void post(String uri, String data) throws Exception {
+  // HttpClient client = HttpClient.newBuilder().build();
+  // HttpRequest request =
+  // HttpRequest.newBuilder().uri(URI.create(uri)).POST(BodyPublishers.ofString(data)).build();
+  // HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
+  // System.out.println(response.statusCode());
+  // }
+
+  // public void postJson(URL uri, Citizen data) throws Exception {
+  // ObjectMapper objectMapper = new ObjectMapper();
+  // String requestBody =
+  // objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+  // String requestBody =
+  // objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+  // HttpRequest request = HttpRequest.newBuilder(uri).header("Content-Type",
+  // "application/json")
+  // .POST(BodyPublishers.ofString(requestBody)).build();
+  // HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
+  // System.out.println(response.statusCode());
+  // }
 }
